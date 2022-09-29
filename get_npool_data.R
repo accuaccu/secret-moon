4909048c1a16ea447d4ccbc1246fc4e66d40be76
accuaccu <paavo.raittinen@gmail.com>
2022-09-29 20:30:59 +0300

    Nordpool scraper

---
+++ get_npool_data.R
@@ -0,0 +1,18 @@
+# Get day-ahead market price from Nordpool
+library(webshot)
+library(tesseract)
+library(rvest)
+
+# Define VAR
+url_npool <- 'https://www.nordpoolgroup.com/en/Market-data1/#/nordic/table'
+str_today <- Sys.Date()
+dir_temp_image <- './image_tmp'
+
+# Get snapshot from URL with 5 second delay and zoom = 2 due to tesseract being unambigious otherwise
+webshot(url_npool, file = paste0(dir_temp_image,'/npool.png'), delay = 5, selector = ".table-wrapper", zoom = 2)
+
+
+str_text <- ocr(image = paste0(dir_temp_image,'/npool.png'))
+cat(str_text, '\n')
+
+
